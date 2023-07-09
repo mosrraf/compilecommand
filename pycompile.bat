c55522fcd5fded1e81282f80d93cd4ff4a711757
Mohamed alaa el_srraf <111457283+mosrraf@users.noreply.github.com>
2023-07-09 21:45:47 +0300

    Update pycompile.bat

--- pycompile.bat
+++ pycompile.bat
@@ -1,3 +1,5 @@
+:restart
+cls
 set /p drive="Please, enter the drive: "
 cls
 %drive%:
@@ -5,9 +7,22 @@ cls
 set /p directory="Please, enter the directory: "
 cls
 cd /%directory%
+:start
 cls
 set /p file="Please, enter the file name: "
+:same
 cls
 python %file%.py
+set /p end="press enter to continue."
+cls
+set /p ans="do you want to recompile program? (y/n) ans: "
+if %ans%==y goto same
+cls
+set /p ans="do you want to compile another program in same path? (y/n) ans: "
+if %ans%==y goto start
+cls
+set /p ans="do you want to compile another program in another path? (y/n) ans:"
+if %ans%==y goto restart
+cls
 set /p end="press enter to continue."
 cls
